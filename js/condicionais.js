d1207d95f55b0c1ceed6dfa5a97880f3280b187c
cristianeasreis <cristianeasreis@hotmail.com>
2020-09-21 13:47:34 -0300

    estuando laços de repetição

---
+++ js/condicionais.js
@@ -0,0 +1,34 @@
+// Condicionais
+
+/*var idade = prompt('Qual sua Idade ?');
+
+if(idade >=18){
+    alert('Maior de Idade');
+}
+else{
+    alert('Menor de Idade');
+}*/
+
+// Laços de Repetição
+
+//While
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    count ++;
+};
+*/
+
+//For
+
+/*var count;
+for (count=0; count<=5; count++) {
+    console.log(count);
+};*/
+
+// Trabalhando com Data
+
+var d = new Date();
+console.log(d.getDate());
+console.log(d.getHours());
