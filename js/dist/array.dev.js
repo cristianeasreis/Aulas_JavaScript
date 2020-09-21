300e3dbbbcf730051588b3fc302b8d57ee47a915
cristianeasreis <cristianeasreis@hotmail.com>
2020-09-21 07:15:36 -0300

    estuando arrays

---
+++ js/dist/array.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Trabalhando Com Array
+//var lista = ["Maçã","Pera","Laranja"];
+// Adicionar um elemento a mais 
+//lista.push("Uva");
+// Deletar um elemento 
+//lista.pop("Uva");
+//console.log(lista);
+//console.log(lista.join(" - ")); // Para tirar a virgula e colocar o que vc quiser
+//console.log(lista.toString()); //Trazer a Lista em String
+//console.log(lista.reverse()); //Reverse 
+//console.log(lista.length); //Tamanho da lista
+//console.log(lista[1]); //Imprimir so uma fruta
+// Dicionario
+
+/*var fruta = {nome: "Maça", cor: "Vermelho"}; // Como se foce Json
+console.log(fruta.nome);*/
+var fruta = [{
+  nome: "Maça",
+  cor: "Vermelho"
+}, {
+  nome: "Uva",
+  cor: "Roxo"
+}]; // Como se foce Json
+
+console.log(fruta);
+alert(fruta[1].nome);
---
+++ js/introjavascript.js
@@ -0,0 +1,13 @@
+// Trabalhar Com Variaveis
+
+/*var nome = "Cristiane Reis";
+alert('Bem Vindo! '+ nome);*/
+
+var nome = "Cristiane Reis";
+var idade = 35;
+var idade2 = 40;
+var frase = "Brasil é o Pior";
+
+console.log (nome);
+console.log (idade + idade2);
+console.log (frase.replace("Brasil", "Coreia")); // replace da para ser usado em alert também
